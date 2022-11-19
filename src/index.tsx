bd1d764d6d48cad367af41ea183ef277719df200
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-11-19 01:30:35 +0000

    Configure absolute imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import './index.css';
 
+import App from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
